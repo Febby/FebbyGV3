1e053776b9b52ce362da128369fb36948c7dc004
Febby <gunawan.febby@gmail.com>
2026-05-16 18:36:25 +0700

    Working on the homepage & nav section

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     './src/**/*.{astro,html,js,jsx,ts,tsx,vue}',
     './public/**/*.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+        heading: ['"Space Grotesk"', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
